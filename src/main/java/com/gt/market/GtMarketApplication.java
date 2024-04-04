3826eb3c882f9640c43eaed3858cd914c40e7667
woose0k <dntjr4733@gmail.com>
2024-04-05 00:27:03 +0900

    env setting

---
+++ src/main/java/com/gt/market/GtMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gt.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtMarketApplication.class, args);
+    }
+
+}
